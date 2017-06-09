55aa38dd487075f27ea8706e8b35cf06ffdf4d28
Divam <dfordivam@gmail.com>
2017-06-09 15:55:45 +0900

    Use overlappable instance to achieve toSum

--- generictry/Main.hs
+++ generictry/Main.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import GHC.Generics
 import Data.Aeson
@@ -26,28 +26,11 @@ instance (ToJSON a, ToJSON b) => ToJSON (AorB a b)
 class (TypeNeq r a) => WebSockerRequest r a where
   toSum :: a -> r
 
--- instance (TypeNeq a b) => WebSockerRequest (AorB a b) a where
---   toSum a = A a
-
-instance (TypeNeq () b, TypeNeq (AorB () b) b) => WebSockerRequest (AorB () b) b where
+instance {-# OVERLAPPABLE #-} (TypeNeq a b, TypeNeq (AorB a b) b) => WebSockerRequest (AorB a b) b where
   toSum b = B b
 
--- instance (TypeNeq a b, WebSockerRequest (AorB () b) b) => WebSockerRequest (AorB a (AorB () b)) b where
---   toSum b = B (toSum b)
-
--- instance (TypeNeq (AorB () b1) b2, TypeNeq b1 b2, TypeNeq (AorB (AorB () b1) b2) b2) => WebSockerRequest (AorB (AorB () b1) b2) b2 where
---   toSum b2 = B b2
-instance (TypeNeq b1 b2, TypeNeq (AorB b1 b2) b2) => WebSockerRequest (AorB b1 b2) b2 where
-  toSum b2 = B b2
-
--- instance (TypeNeq a b, TypeNeq (AorB a c) b, WebSockerRequest c b, TypeNeq (AorB () b) c, TypeNeq a ()) => WebSockerRequest (AorB a c) b where
---   toSum b = B (toSum b :: c)
-
--- instance (TypeNeq a b, TypeNeq b c, WebSockerRequest (AorB b c) c) => WebSockerRequest (AorB a (AorB b c)) c where
---   toSum c = B (toSum c)
-
--- instance (TypeNeq a b) => WebSockerRequest (AorB a (AorB b c)) b where
---   toSum b = B b
+instance {-# OVERLAPPABLE #-} (WebSockerRequest a c, TypeNeq (AorB a b) c) => WebSockerRequest (AorB a b) c where
+  toSum c = A (toSum c)
 
 main = do
   let ab1 = [A (2 ::Int) , B "dd"]
