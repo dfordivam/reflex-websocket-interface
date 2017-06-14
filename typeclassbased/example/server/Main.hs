e75174d9b614de57d37e87cb6ebdbc315d19239b
Divam <dfordivam@gmail.com>
2017-06-14 13:03:12 +0900

    Add constraints on handler to be complete

--- typeclassbased/example/server/Main.hs
+++ typeclassbased/example/server/Main.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
 
 module Main where
 
@@ -44,3 +46,39 @@ getResponse' a = head $ catMaybes
 getResponse1 (Request1 t) = Response1 (T.length t)
 
 getResponse2 (Request2 (t1,t2)) = Response2 (t1 <> t2)
+
+data a :<&> b = a :<&> b
+
+data Handler m req a where
+  Handler :: (WebSocketMessage req a, Monad m) =>
+    (req -> a -> m (ResponseT req a )) -> Handler m req a
+
+-- Handler m req a -> Handler m req b -> JoinedHandler m req (a :<|> b)
+-- Handler m req a -> JoinedHandler m req (b :<|> c) -> JoinedHandler m req (a :<|> (b :<|> c))
+
+-- handler :: (WebSocketMessage req a) => (a :<|> b) -> (a -> m (ResponseT req a)) -> m (Maybe Value)
+
+-- combinedGetResponse = getResponse1 :<&> getResponse2 :<&> getResponse3
+
+-- joinHandler :: (WebSocketMessage req a, WebSocketMessage req b) => (a -> m (ResponseT req a)) :<&> b ->
+-- joinHandler (getRespA :<&> (getRespB :<&> other)) = joinHandler other
+-- joinHandler (getRespA :<&> (getRespB :<&> other)) = joinHandler other
+
+class IsValidHandler req h where
+
+instance (WebSocketMessage req a
+  , IsValidHandler b c) => IsValidHandler (a :<|> b) ((Handler m req a) :<&> c)
+
+instance (WebSocketMessage req a) => IsValidHandler a (Handler m req a)
+
+getResp1 :: Shared.Request -> Request1 -> IO Response1
+getResp1 _ (Request1 t) = return $ Response1 (T.length t)
+
+getResp2 :: Shared.Request -> Request2 -> IO Response2
+getResp2 _ (Request2 (t1,t2)) = return $ Response2 (t1 <> t2)
+
+runHandler :: (IsValidHandler req h) => req -> h -> m ()
+runHandler = undefined
+
+runMain :: Shared.Request -> m ()
+runMain val = runHandler val ((Handler getResp1) :<&> (Handler getResp2))
