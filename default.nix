779b7b296fef89b2a686aa4342b93a294871d351
Divam <dfordivam@gmail.com>
2017-11-10 15:34:06 +0900

    Fix default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-haskellPackages: {
+haskellPackages: rec {
     reflex-websocket-interface = haskellPackages.callPackage ./reflex {inherit reflex-websocket-interface-shared;};
     reflex-websocket-interface-server = haskellPackages.callPackage ./server {inherit reflex-websocket-interface-shared;};
     reflex-websocket-interface-shared = haskellPackages.callPackage ./shared {};
